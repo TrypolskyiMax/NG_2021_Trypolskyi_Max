fb4e025de81733c6193b1d2148894e611d2cd0d4
TrypolskyiMax <trypolskyimax@gmail.com>
2021-10-17 15:57:18 +0300

    task2

---
+++ Lesson 1/Task 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout <<"    _______\n";
+   cout <<"   /      /,\n";
+   cout <<"  /      //\n";
+   cout <<" /______//\n";
+   cout <<"(______(/\n";
+   return 0;
+}
